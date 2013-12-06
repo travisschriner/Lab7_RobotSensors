1e380a7df0aa75de795e253caf661a1f103ad6eb
Travis Schriner <C15Travis.Schriner@usafa.edu>
2013-12-05 17:27:51 -0700

    correct code cleaned up

--- main.c
+++ main.c
@@ -1,30 +1,21 @@
-#include <msp430.h> 
+#include <msp430g2553.h>
 #include "sensors.h"
-
-/*
- * main.c
- */
-
-
-
 
 int main(void)
 {
-  WDTCTL = WDTPW + WDTHOLD;                 // Stop WDT
-  ADC10CTL0 = ADC10SHT_3 + ADC10ON + ADC10IE; // ADC10ON, interrupt enabled
-  ADC10CTL1 = INCH_4;                       // input A4
-  ADC10AE0 |= BIT4;                         // PA.1 ADC option select
-  ADC10CTL1 |= ADC10SSEL1|ADC10SSEL0;                // Select SMCLK
-  P1DIR |= BIT6;                            // Set P1.0 to output direction
+
+  initialize();
 
   for (;;)
   {
-    ADC10CTL0 |= ENC + ADC10SC;             // Sampling and conversion start
-    __bis_SR_register(CPUOFF + GIE);        // LPM0, ADC10_ISR will force exit
-    if (ADC10MEM < 0x1FF)
-      P1OUT &= ~(BIT6);                       // Clear P1.0 LED off
-    else
-      P1OUT |= (BIT6);                        // Set P1.0 LED on
+
+	leftSensor();
+    leftWall(0x1D0);
+
+    rightSensor();
+    rightWall(0x1D0);
+
+
   }
 
     return 0;
@@ -36,3 +27,4 @@ __interrupt void ADC10_ISR(void)
 {
   __bic_SR_register_on_exit(CPUOFF);        // Clear CPUOFF bit from 0(SR)
 }
+
--- sensors.h
+++ sensors.h
@@ -2,12 +2,65 @@
  * sensors.h
  *
  *  Created on: Dec 4, 2013
- *      Author: C15Travis.Schriner
+ *  Author: C15Travis.Schriner
  */
 
 #ifndef SENSORS_H_
 #define SENSORS_H_
 
+void initialize();
+
+void leftSensor();
+
+void rightSensor();
+
+void centerSensor();
+
+/*
+ * Function: leftWall(int threshhold)
+ *
+ * Description: This function will return a 1 if the
+ * 				sensor on pin 3 senses a wall. It will
+ * 				return a 0 if no wall is detected.
+ * 				It will also turn on the LED on P1.0
+ * 				as long as the sensor detects a wall.
+ *
+ * 	Inputs: threshhold~ this is a user defined threshhold
+ * 						level that can be calibrated
+ * 						depending on the sensor, lighting,
+ * 						wall, and many other factors.
+ */
+int leftWall(int threshhold);
+
+/*
+ * Function: rightWall(int threshhold)
+ *
+ * Description: This function will return a 1 if the
+ * 				sensor on pin 5 senses a wall. It will
+ * 				return a 0 if no wall is detected.
+ * 				It will also turn on the LED on P1.6
+ * 				as long as the sensor detects a wall.
+ *
+ * 	Inputs: threshhold~ this is a user defined threshhold
+ * 						level that can be calibrated
+ * 						depending on the sensor, lighting,
+ * 						wall, and many other factors.
+ */
+int rightWall(int threshhold);
+
+/*
+ * Function: frontWall(int threshhold)
+ *
+ * Description: This function will return a 1 if the
+ * 				sensor on pin 4 senses a wall. It will
+ * 				return a 0 if no wall is detected.
+ *
+ * 	Inputs: threshhold~ this is a user defined threshhold
+ * 						level that can be calibrated
+ * 						depending on the sensor, lighting,
+ * 						wall, and many other factors.
+ */
+int frontWall(int threshhold);
 
 
 #endif /* SENSORS_H_ */
